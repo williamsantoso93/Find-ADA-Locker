1b0676c9816dd6451b7d4b907ee7035e7066ae6a
William Santoso <williamsantoso93@yahoo.com>
2019-09-18 18:51:12 +0700

    Initial Commit

---
+++ Find ADA Locker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Find ADA Locker
+//
+//  Created by William Santoso on 18/09/19.
+//  Copyright © 2019 William Santoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Find ADA LockerTests/Find_ADA_LockerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Find_ADA_LockerTests.swift
+//  Find ADA LockerTests
+//
+//  Created by William Santoso on 18/09/19.
+//  Copyright © 2019 William Santoso. All rights reserved.
+//
+
+import XCTest
+@testable import Find_ADA_Locker
+
+class Find_ADA_LockerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
